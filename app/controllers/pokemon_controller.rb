1fd77e403a04e2de329fc3bdb4c4fb7fda7268cb
Tanner Dale <tanner@tannerdale.com>
2021-09-07 23:27:12 -0500

    Functioning with poros

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,5 +1,9 @@
 class PokemonController < ApplicationController
   def index
     pokemon = params[:pokemon]
+    conn = Faraday.new("https://pokeapi.co")
+    response = conn.get("/api/v2/pokemon-form/#{pokemon}/")
+    data = JSON.parse(response.body, symbolize_names: true)
+    @pokemon = Pokemon.new(data)
   end
 end
---
+++ app/poros/pokemon.rb
@@ -0,0 +1,9 @@
+class Pokemon
+  attr_reader :name, :number, :image
+
+  def initialize(data)
+    @name = data[:name].capitalize
+    @number = data[:id]
+    @image = data[:sprites][:front_default]
+  end
+end
